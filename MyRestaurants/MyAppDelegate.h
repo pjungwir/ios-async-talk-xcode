f4fcc38b3337b4c4baf6ceab10335796f11f6ad0
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2014-03-02 14:45:36 -0800

    Initial Commit

---
+++ MyRestaurants/MyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyAppDelegate.h
+//  MyRestaurants
+//
+//  Created by Arielle Jungwirth on 3/2/14.
+//  Copyright (c) 2014 Paul A. Jungwirth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyRestaurants/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyRestaurants
+//
+//  Created by Arielle Jungwirth on 3/2/14.
+//  Copyright (c) 2014 Paul A. Jungwirth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyAppDelegate class]));
+    }
+}
